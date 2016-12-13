f37f19f4fd163efd9fa2c07dbfc7b2684ef15490
Moose <mooseuniversity@yahoo.com>
2016-12-13 14:06:21 -0600

    Remove onscreen effect

--- js/onscreen.js
+++
@@ -1,7 +0,0 @@
-var wrapper = document.querySelector('.animate');
-window.onscroll = function (event) {
-    if (wrapper.getBoundingClientRect().top < 0) {
-        wrapper.className = "animate onScreen";
-        window.onscroll = null;
-    }
-}
